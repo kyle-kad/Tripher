e6e0799711efe8d70cd3c50864ac07172370351d
kyle <kadangotinaishe5@gmail.com>
2020-08-10 09:38:43 +0200

    Working App with user info reducer added

--- frontend/src/store/modules/rootReducer.ts
+++ frontend/src/store/modules/rootReducer.ts
@@ -4,9 +4,10 @@ import intrests  from "./intrests/reducer";
 import subscriptions from "./subscriptions/reducer";
 import featured from "./featured/reducer";
 import recents from "./recents/reducer"
+import user from "./user/reducer"
 
 export const rootReducer = combineReducers({
-    intrests, subscriptions, featured, recents
+    intrests, subscriptions, featured, recents, user
 })
 
 export type RootState = ReturnType<typeof rootReducer>;
