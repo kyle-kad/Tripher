145afaa397d0bcc35eee32220a087d27851f531f
Kyle Kadango <45475970+NoobKyle@users.noreply.github.com>
2020-12-14 13:22:16 +0200

    Update Login.tsx

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -10,14 +10,14 @@ import LoginComp from './components/LoginComp/LoginComp';
 const Login: React.FC = () => {
 
 	var firebaseConfig:any  = {
-	    apiKey: "AIzaSyCOPq2Z3mV05l__KYnapmFVOwuVDX8U9Do",
-	    authDomain: "tripher-a8610.firebaseapp.com",
-	    databaseURL: "https://tripher-a8610.firebaseio.com",
-	    projectId: "tripher-a8610",
-	    storageBucket: "tripher-a8610.appspot.com",
-	    messagingSenderId: "392936344431",
-	    appId: "1:392936344431:web:a9a10f8ce117071a2e369a",
-		measurementId: "G-LQ4G4MPNDL"
+	    apiKey: "",
+	    authDomain: "",
+	    databaseURL: "",
+	    projectId: "",
+	    storageBucket: "",
+	    messagingSenderId: "",
+	    appId: "",
+		measurementId: ""
 	};
 
  	
